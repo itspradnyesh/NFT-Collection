231b4aea78b3e42b12f9475b66dab6d7abfb5e6b
Pradnyesh banait <banait.pradnyesh9008@gmail.com>
2022-10-11 17:04:15 +0530

    init

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x0918e8CE21aADF042b791Ae0A7e9e18CC774215e";
+const WHITELIST_CONTRACT_ADDRESS = "0x930Bcd6B46B44475912C442085BEe50Bf3B473A1";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://whitelist-dapp-alpha-beryl.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
